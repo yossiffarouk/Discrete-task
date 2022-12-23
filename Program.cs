60451a1e48a05e011dfeadee453696f5117a35d3
yossiffarouk <121296343+yossiffarouk@users.noreply.github.com>
2022-12-23 14:43:23 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,40 +1,39 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp5
+namespace ConsoleApp4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int n, i, sum;
-            int y,x;
-
-            Console.WriteLine("Input the starting of numbers");
-            x = int.Parse(Console.ReadLine());
-            Console.WriteLine("Input the ending of numbers ");
-            y = int.Parse(Console.ReadLine());
-            Console.Write("The Perfect numbers within the given range : ");
-            for (n = x; n <= y; n++)
+            Console.WriteLine("enter first range number");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter last range number");
+            int y =int.Parse(Console.ReadLine());
+            bool prime = true;
+            int j ;
+            for (int i = x; i <= y; i++)
             {
-                i = 1;
-                sum = 0;
-                while (i < n)
+                for ( j = 1; j < i; j++)
                 {
-                    if (n % i == 0)
-                        sum = sum + i;
-                    i++;
+                    if (i%j==0)
+                    {
+                        prime = false;
+                    }
                 }
-                if (sum == n)
-                    Console.Write("{0} \t ", n);
+                if (prime == true)
+                {
+                    Console.WriteLine("the prime numbers are :" + j);
+                }
+                else prime = true;
             }
-            
-            
-            Console.ReadKey();  
+            Console.ReadKey();
         } 
     }
-    
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp5")]
+[assembly: AssemblyTitle("ConsoleApp4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp5")]
+[assembly: AssemblyProduct("ConsoleApp4")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("154e1c3f-c31c-4f1b-9436-7edddb4140cd")]
+[assembly: Guid("eb9aa7f6-e6b5-4531-8c58-19301f2b3182")]
 
 // Version information for an assembly consists of the following four values:
 //
